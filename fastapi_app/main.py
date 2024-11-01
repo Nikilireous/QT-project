36a799c47ef629d471483dc521a0e989d99be246
Nikita Golubkov <golubkoffnickita0@gmail.com>
2024-11-01 20:15:31 +0300

    feat: add `GET /posters`

--- fastapi_app/main.py
+++ fastapi_app/main.py
@@ -1,11 +1,40 @@
 from fastapi import FastAPI, HTTPException, status
 from email_validator import validate_email, EmailNotValidError
 from bcrypt import hashpw, checkpw, gensalt
+import sqlite3
 
 
 app = FastAPI()
+absolute_path = "fastapi_app/"
 
 
 @app.get("/")
 async def index():
     return {"works": True}
+
+
+@app.get("/posters")
+async def get_existing_posters():
+    answer = {}
+    sql_request = '''SELECT
+                        Poster.CompositionId,
+                        Poster.Title,
+                        Genre.GenreName,
+                        Poster.Time,
+                        Theatres.TheatreName,
+                        Poster.Price
+                    FROM
+                            Poster
+                        LEFT JOIN Genre ON Poster.GenreId = Genre.GenreId
+                        LEFT JOIN Theatres ON Poster.TheatreId = Theatres.TheatreId'''
+
+    con = sqlite3.connect(f"{absolute_path}all_data/poster_data.db")
+    cur = con.cursor()
+
+    poster_table = cur.execute(sql_request).fetchall()
+    con.close()
+
+    for row in poster_table:
+        answer[row[0]] = row[1:]
+
+    return answer
